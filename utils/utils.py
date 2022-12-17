3b039a668c248040a2f539eb52f3bf58e025c960
hi-i-m-gtooth <noface21105@gmail.com>
2022-12-17 23:40:25 +0800

    init utils.py - general/remap/infer

---
+++ utils/utils.py
@@ -0,0 +1,46 @@
+################# General tool #################
+import pickle
+
+# quick read file
+def o(path):
+    return open(path, 'r')
+
+# quick pickle I/O
+def out_pkl(data, path):
+    pickle.dump(data, open(path, 'wb'))
+def load_pkl(path):
+    return pickle.load(open(path, 'rb'))
+
+################# Remap #################
+
+def get_remaps(_type):
+    """
+    Parmeters: 
+    _type: str - Selected remap type
+
+    Return: 
+    id2name: dict()
+    name2id: dict()
+    """
+    _type = _type.lower()
+    assert _type in ["course", "c", "subgroup", "g", "user", "u"], 'Available remap type: ["course", "c", "subgroup", "g", "user", "u"]'
+    if _type == "c":
+        _type = "course"
+    elif _type == "g":
+        _type = "subgroup"
+    elif _type == "u":
+        _type = "user"
+    return load_pkl(f"./remap/{_type}_id2name.pkl"), load_pkl(f"./remap/{_type}_name2id.pkl")
+
+################# Inference #################
+
+def get_test_userlist(seen=True):
+    """
+    Parameters:
+    seen: bool - Select seen / unseen user list
+
+    Return:
+    user_list: list()
+    """
+    seen_or_not = "seen" if seen else "unseen"
+    return load_pkl(f"./inference/{seen_or_not}_users_list.pkl")
