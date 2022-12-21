da1d3c6cc776682c56a52dfde373366ae99fb123
hi-i-m-gtooth <noface21105@gmail.com>
2022-12-21 13:01:14 +0800

    update LGN -- yzliu@1221

--- LightGCN-PyTorch/code/run_hahow_pred.sh
+++ LightGCN-PyTorch/code/run_hahow_pred.sh
@@ -1,4 +1,7 @@
-python hahow_pred.py --decay=1e-4 --lr=0.001 --layer=2 --seed=7777 --dataset="hahow_course" --topks="[10,20,50]" --recdim=1024 --bpr_batch 12288 --testbatch 500 --epochs 5000 \
-                    -w lgn-hahow_course-2-1024/lgn-hahow_course-2-1024_3950ep.pth.tar
-#python hahow_pred.py --decay=1e-4 --lr=0.001 --layer=2 --seed=7777 --dataset="hahow_group" --topks="[10,20,50]" --recdim=1024 --bpr_batch 12288 --testbatch 500 --epochs 5000 \
-#                    -w lgn-hahow_group-2-1024/lgn-hahow_group-2-1024_3950ep.pth.tar
+ep=4050
+layer=2
+dim=1024
+task='group'
+
+python hahow_pred.py --decay=1e-4 --lr=0.001 --layer=${layer} --seed=7777 --dataset="hahow_${task}" --topks="[10,20,50]" --recdim=${dim} --bpr_batch 12288 --testbatch 500 --epochs 5000 \
+                     -w lgn-hahow_${task}-${layer}-${dim}/lgn-hahow_${task}-${layer}-${dim}_${ep}ep.pth.tar
