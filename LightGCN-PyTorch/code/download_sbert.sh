b307c2f1de8db61136caaae17be3de08a23effc6
hi-i-m-gtooth <noface21105@gmail.com>
2023-01-03 16:06:54 +0800

    download for sbert embed -- yzliu@0103

---
+++ LightGCN-PyTorch/code/download_sbert.sh
@@ -0,0 +1,3 @@
+wget https://www.dropbox.com/s/ky5x0huinqlt7s8/user_embed.np.pkl?dl=1 -O './embeds/user_embed.np.pkl'
+gdown 'https://drive.google.com/uc?id=1FB_uj_W0dN_5ldrgpjEIyTLUNw7Hhnpj' -O './embeds/course_embed.np.pkl'
+gdown 'https://drive.google.com/uc?id=1GKvgYdP9CSybPmeVbarVArZ0AnaVGR0u' -O './embeds/group_embed.np.pkl'
