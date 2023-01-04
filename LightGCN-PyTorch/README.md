64b87106794a0714bc230380892d45de6942e1c7
hi-i-m-gtooth <noface21105@gmail.com>
2023-01-04 17:56:44 +0800

    hard predict for reproduce -- yzliu@0104

--- LightGCN-PyTorch/README.md
+++ LightGCN-PyTorch/README.md
@@ -9,30 +9,30 @@ pip install -r requirements.txt
 #### Download
 Download embeddings encoded by Sbert.
 ```bash
-cd code
-./download_sbert.sh
+pushd code; ./download_sbert.sh; popd
 ```
 
 #### Train
-
+Models will be stored in `checkpoints`.
 ```bash
-cd code
-./run_seen_course_train.sh
-./run_unseen_course_train_sbert.sh
-./run_seen_group_train.sh
-./run_unseen_group_train_sbert.sh
+pushd code; ./run_seen_course_train.sh; ./run_unseen_course_train_sbert.sh; ./run_seen_group_train.sh; ./run_unseen_group_train_sbert.sh; popd
 ```
 
 #### Assemble score
 Prepared for assemble / ensemble method.
 ```bash
-cd code
-./run_hahow_all_assemble_score.sh
+pushd code; ./run_hahow_all_assemble_score.sh; popd
 ```
 
 ### Prediction
-Note that you need to change the dataset & hyperparms, respectively for **course** & **group**.
+Predictions will be stored in `preds`
+#### From scratch
+Note that if you trained with different settings, you'll need to change the dataset & hyperparms in `run_hahow_pred.sh`.
 ```bash
-cd code
-./run_hahow_pred.sh
+pushd code; ./run_hahow_pred.sh; popd
+```
+#### From pretrained
+```bash
+pushd ..; ./lgn_download.sh; popd # You will see ../models if you successfully download them
+pushd code; ./run_hahow_pred_pretrained.sh; popd
 ```
--- LightGCN-PyTorch/code/run_hahow_pred.sh
+++ LightGCN-PyTorch/code/run_hahow_pred.sh
@@ -1,7 +1,31 @@
-ep=15000
+ep=2150
+layer=5
+dim=1024
+task='course'
+
+python hahow_pred.py --decay=1e-4 --lr=0.001 --layer=${layer} --seed=7777 --dataset="hahow_${task}" --topks="[10,20,50]" --recdim=${dim} --bpr_batch 12288 --testbatch 500 --epochs 5000 \
+                     -w lgn-hahow_${task}-${layer}-${dim}/lgn-hahow_${task}-${layer}-${dim}_${ep}ep.pth.tar
+
+ep=20000
 layer=2
 dim=768
-#task='course'
+task='course'
+
+python hahow_pred.py --decay=1e-4 --lr=0.001 --layer=${layer} --seed=7777 --dataset="hahow_${task}" --topks="[10,20,50]" --recdim=${dim} --bpr_batch 12288 --testbatch 500 --epochs 5000 \
+                     -w lgn-hahow_${task}-${layer}-${dim}/lgn-hahow_${task}-${layer}-${dim}_${ep}ep.pth.tar \
+                     --cos
+
+ep=250
+layer=3
+dim=1024
+task='group'
+
+python hahow_pred.py --decay=1e-4 --lr=0.001 --layer=${layer} --seed=7777 --dataset="hahow_${task}" --topks="[10,20,50]" --recdim=${dim} --bpr_batch 12288 --testbatch 500 --epochs 5000 \
+                     -w lgn-hahow_${task}-${layer}-${dim}/lgn-hahow_${task}-${layer}-${dim}_${ep}ep.pth.tar
+
+ep=7000
+layer=2
+dim=768
 task='group'
 
 python hahow_pred.py --decay=1e-4 --lr=0.001 --layer=${layer} --seed=7777 --dataset="hahow_${task}" --topks="[10,20,50]" --recdim=${dim} --bpr_batch 12288 --testbatch 500 --epochs 5000 \
