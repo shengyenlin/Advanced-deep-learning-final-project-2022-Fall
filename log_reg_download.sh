8be759f6dace2b39e057741bad0fe9a7f1072ccd
shengyenlin <shengyenlin0501@gmail.com>
2023-01-05 17:08:47 +0800

    update download link

--- log_reg_download.sh
+++ log_reg_download.sh
@@ -4,9 +4,12 @@ unzip cache.zip -d logistic_regression
 rm -f cache.zip
 
 # download dictionary
-wget -O lg_dict.zip https://www.dropbox.com/s/tkrk34bjieyql0n/lg_dict.zip?dl=1
-unzip ./lg_dict.zip -d ./ensemble/data
-rm -f lg_dict.zip
+wget -O lg_group.zip https://www.dropbox.com/s/xi2xmn0zxvlyaq5/lg_group_score.dict.pkl.zip?dl=1
+wget -O lg_course.zip https://www.dropbox.com/s/ljkchlsxyoi0xe2/lg_course_score.dict.pkl.zip?dl=1
+unzip lg_group.zip -d ./ensemble/data
+unzip lg_course.zip -d ./ensemble/data
+rm -f lg_group.zip
+rm -f lg_course.zip
 
 # download fasttext
 cd logistic_regression && \
