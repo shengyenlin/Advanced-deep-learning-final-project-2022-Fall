521be9e7b410e2a57b510930d9f9da0eaff2b8cd
hi-i-m-gtooth <noface21105@gmail.com>
2023-01-05 22:16:14 +0800

    dl for directly ensemble -- yzliu@0105

--- lgn_download.sh
+++ lgn_download.sh
@@ -1,2 +1,4 @@
+wget https://www.dropbox.com/s/eegyv4be61zch37/lgn_course_score.dict.pkl -P './ensemble/data'
+wget https://www.dropbox.com/s/iq88ozk5b7mv0i7/lgn_group_score.dict.pkl -P './ensemble/data'
 wget https://www.dropbox.com/s/seykmo99fcw8dpg/models.zip?dl=1 -O ./LightGCN-PyTorch/models.zip
 unzip ./LightGCN-PyTorch/models.zip -d ./LightGCN-PyTorch/
