d2a1d43710bcf1926159bc7cb41d2befd98a4d63
morriscsie <64307160+morriscsie@users.noreply.github.com>
2023-01-05 19:02:06 +0800

    Update als_download.sh

--- als_download.sh
+++ als_download.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-wget https://www.dropbox.com/s/4c0u3pi82hl210z/als_course_score.dict.zip?dl=1 -O ./als_course_score.dict.zip
+wget https://www.dropbox.com/s/wx7h81e4xinn78l/als_course_score.dict.zip?dl=1 -O ./als_course_score.dict.zip
 unzip ./als_course_score.dict.zip -d ./ensemble/data
 rm -f ./als_course_score.dict.zip
